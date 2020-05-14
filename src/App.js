913ea587ef7d545e3579807857888b4b67a4be0e
saksham695 <saksham695@gmail.com>
2020-05-14 12:51:29 +0530

    reacr-form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React, { Component } from "react";
+import "./App.css";
+export default class App extends Component {
+  resetPressed = () => {
+    console.log("Reset Pressed");
+  };
+  submitPressed = () => {
+    console.log("Submit Pressed");
+  };
+  render() {
+    return (
+      <div className="container">
+        <h4>Login</h4>
+        <form>
+          <label>
+            Name
+            <input type="text" placeholder="Name" className="input1" />
+          </label>
+          <br></br>
+          <label>
+            Password
+            <input type="text" placeholder="Password" className="input2" />
+          </label>
+        </form>
+        <input
+          className="button"
+          type="button"
+          value="Reset"
+          onClick={this.resetPressed}
+        />
+        <input
+          className="button"
+          type="button"
+          value="Submit"
+          onClick={this.submitPressed}
+        />
+      </div>
+    );
+  }
 }
-
-export default App;
